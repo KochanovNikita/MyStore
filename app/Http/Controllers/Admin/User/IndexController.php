d0692dd6d72346c3837edc8eadcf2030b3c1983f
kovhanovn <kochanovn207@gmail.com>
2022-08-02 19:16:42 +0300

    feat: add users variable

--- app/Http/Controllers/Admin/User/IndexController.php
+++ app/Http/Controllers/Admin/User/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -15,6 +16,7 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('admin.user.index');
+        $users = User::all();
+        return view('admin.user.index', compact('users'));
     }
 }
